edbb06695fc1695da732b631fa24d8c0f569cce4
Kapicka <hanus.oh@gmail.com>
2023-03-23 00:46:39 +0100

    add Interfaces IImage, IProgram and IListItem

--- src/interface.ts
+++ src/interface.ts
@@ -1,6 +1,32 @@
+import ProgramTypeEnum from "./enum/ProgramTypeEnum";
+
 export interface IListItem {
-    id: number,
-    title: string
-    img: string
-    coverText?: string
+  id: number;
+  title: string;
+  image: IImage;
+  coverText?: string;
+}
+
+export interface IProgram {
+  title: string;
+  description: string;
+  programType: string;
+  images: {
+    [key: string]: {
+      url: string;
+      width: number;
+      height: number;
+    };
+  };
+  releaseYear: number;
+}
+
+export interface IImage {
+  url: string;
+  width?: number;
+  height?: number;
+  srcset?: string;
+  sizes?: string;
+  //placeholder for full size image
+  loadingImage?: string;
 }
