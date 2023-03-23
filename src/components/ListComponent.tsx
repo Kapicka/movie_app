0fe55200772af9306ed38dd3ece22c6a3bb35759
Kapicka <hanus.oh@gmail.com>
2023-03-23 08:50:46 +0100

    reformat code

--- src/components/ListComponent.tsx
+++ src/components/ListComponent.tsx
@@ -1,35 +1,44 @@
-import React from 'react';
-import ListItem, {ListItemProp} from "./ListItem";
-import {IListItem} from "../interface";
-
+import React from "react";
+import ListItem from "./ListItem";
+import { IListItem } from "../interface";
 
 type Prop = {
-    className?: string
-    title: string
-    items: IListItem[]
-    onItemClick: (id: number) => void
-}
-const ListComponent = ({className = '', title, items = [], onItemClick}: Prop) => {
-    return (
-        <div className="block">
-            <div className="flex justify-center items-center w-full px-10 bg-gray-700 h-[60px] ">
-                <div
-                    className="w-full text-start text-lg font-bold  text-white text-lg font-bold max-w-[900px] ">{title}</div>
-            </div>
-
-            <div className="flex justify-center items-center w-full px-10]">
-                <div className="flex gap-4 w-full text-start text-lg max-w-[900px] mt-4 ">
-                    {items.map(item => <div><ListItem
-                        onClick={() => onItemClick(item.id)}
-                        id={item.id}
-                        title={item.title}
-                        img={item.img}
-                        coverText={item.coverText}/>
-                    </div>)}
-                </div>
-            </div>
+  className?: string;
+  title: string;
+  items: IListItem[];
+  onItemClick: (id: number) => void;
+};
+const ListComponent = ({
+  className = "",
+  title,
+  items = [],
+  onItemClick,
+}: Prop) => {
+  return (
+    <div className={`${className} block`}>
+      <div className="flex h-[60px] w-full items-center justify-center bg-gray-700 px-10 ">
+        <div className="w-full max-w-[900px] text-start text-lg  text-lg font-bold font-bold text-white ">
+          {title}
         </div>
-    );
+      </div>
+      <div className="px-10] flex w-full items-center justify-center">
+        <div className="mt-4 grid w-full w-full max-w-[900px] grid-cols-2 gap-4 px-4 text-start text-lg sm:grid-cols-4 md:grid-cols-5 ">
+          {items.map((item) => (
+            <div>
+              <ListItem
+                className="h-full w-full"
+                onClick={() => onItemClick(item.id)}
+                id={item.id}
+                title={item.title}
+                img={item.image}
+                coverText={item.coverText}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ListComponent;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import {NavLink} from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 type Prop = {
-    className?: string
-}
-const Navbar = ({className = ''}: Prop) => {
-    return (
-        <div className="flex justify-center  w-full px-10 bg-blue-600 h-[60px] ">
-            <nav className={`${className} flex items-center justify-between max-w-[900px] w-full`}>
-                <NavLink className="link text-white size text-lg font-bold" to="/">DEMO Streaming</NavLink>
-                <div className="flex gap-8 items-center">
-                    <NavLink className="link text-white" to={'/login'}>Log in</NavLink>
-                    <NavLink className="link bg-gray-700 text-white px-4 py-1" to={'/free-trial'}>Start your free
-                        trial</NavLink>
-                </div>
-            </nav>
+  className?: string;
+};
+const Navbar = ({ className = "" }: Prop) => {
+  return (
+    <nav
+      className={`${className} flex h-[60px] w-full justify-center bg-blue-600 px-10`}
+    >
+      <ul className={`flex w-full max-w-[900px] items-center justify-between`}>
+        <NavLink className="link size text-xl font-bold text-white" to="/">
+          DEMO Streaming
+        </NavLink>
+        <div className="flex items-center gap-8">
+          <NavLink className="link text-white" to={"/login"}>
+            Log in
+          </NavLink>
+          <NavLink
+            className="link hidden bg-gray-700 px-4 py-1 text-white sm:block"
+            to={"/free-trial"}
+          >
+            Start your free trial
+          </NavLink>
         </div>
-    );
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
