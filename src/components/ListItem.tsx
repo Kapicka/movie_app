ccf171b94dbf5174edaeaa3de232b924417e06e4
Kapicka <hanus.oh@gmail.com>
2023-03-23 08:48:30 +0100

    replace img element with custom ImageComponent in ListItem

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
+import React from "react";
+import { IImage } from "../interface";
+import ImageComponent from "./ImageComponent";
 
 export type ListItemProp = {
-    id: number,
-    className?: string
-    img: string
-    coverText?: string
-    title: string
-    onClick?: (arg: any) => void
-}
-const ListComponent = ({className = '', title = "koko", img, coverText, onClick}: ListItemProp) => {
-    return (
-        <div onClick={onClick} className="cursor-pointer">
-            <div className="w-[150px] h-[200px] relative">
-                <img src={img} className="absolute top-0 h-full w-full left-0  bg-cover bg-no-repeat object-cover"/>
-                {coverText && <div
-                    className="absolute flex items-center justify-center z-10 w-full h-full bg-cover bg-no-repeat bg-black opacity-80">
-                    <p className="text-3xl font-bold text-white"> {coverText}</p>
-                </div>}
-            </div>
-            <div>{title}</div>
-        </div>
-    );
+  id: number;
+  className?: string;
+  img: IImage;
+  coverText?: string;
+  title: string;
+  onClick?: (arg: any) => void;
+};
+const ListComponent = ({
+  className = "",
+  title,
+  img,
+  coverText,
+  onClick,
+}: ListItemProp) => {
+  return (
+    <div
+      {...(onClick && { onClick })}
+      className={`${className} cursor-pointer`}
+    >
+      <div className="relative">
+        <ImageComponent
+          image={img}
+          className="h-full w-full  bg-cover bg-no-repeat object-cover"
+        />
+        {coverText && (
+          <div className="absolute top-0 left-0 z-10 flex h-full w-full items-center justify-center bg-black bg-cover bg-no-repeat opacity-80">
+            <p className="text-3xl font-bold text-white"> {coverText}</p>
+          </div>
+        )}
+      </div>
+      <div>{title}</div>
+    </div>
+  );
 };
 
 export default ListComponent;
